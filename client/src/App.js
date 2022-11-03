016623cfd569f18fc0e60b7970febcaed2fcec29
Charles Walker <walkercharles100@gmail.com>
2022-11-02 19:40:03 -0500

    start point for website

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Pom & Honey 4 Lyfe </h1>
+
+      <div className="RegisterCustomer">
+        <label> Username </label>
+        <input type="text" name="username" />
+        <label> Password </label>
+        <input type="text" name="password" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
